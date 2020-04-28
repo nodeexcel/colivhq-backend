6f5a189e535467b36234a10b892cc729a1fa9a2a
nodeexcel <nodeexcel@gmail.com>
2020-04-28 15:58:17 +0530

    remove files

--- functions/routes/user.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const userController = require('../controller/user');
-const auth = require('../middleware/auth');
-
-router.post('/register', userController.addUser);
-
-module.exports = router
